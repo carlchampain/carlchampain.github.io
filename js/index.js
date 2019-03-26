eb0bd3a87c8dea9d1df3289bd3f96e2f29fa04ac
Carl Champain <carl_champain@Carls-MacBook-Pro.local>
2019-03-26 15:13:48 -0400

    test fro sunglasses in navbar

--- js/index.js
+++ js/index.js
@@ -127,7 +127,7 @@ function onLoadCreateHome() {
     makeElement("img", { className: "imgHome", id: "moi", src: "./moi.png" }, "");
     makeElement("h4", { className: "title" }, "This is a single page app built with plain javascript and css.");
     makeElement("h4", { className: "title" }, "I am a full stack web developer living in Brooklyn, NY.");
-    makeElement("img", { className: "imgHome", id: "sunglasses", src: "./sunglasses.png" }, "");
+    // makeElement("img", { className: "imgHome", id: "sunglasses", src: "./sunglasses.png" }, "");
     makeElement("h1", { className: "title" }, homeTextTitle);
     loadForHomeImg();
 }
